2e60aebb4c1ff8a52b06da3beac4431aeb09c6e4
awabofficial <awab.teto.official@gmail.com>
2024-10-12 21:05:07 +0300

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const DATABASE_URL =
   process.env.DATABASE_URL === undefined ? databasePath : process.env.DATABASE_URL
 module.exports = {
   VERSION: require('./package.json').version,
-  SESSION_ID: (process.env.SESSION_ID || '').trim(),
+  SESSION_ID: (process.env.SESSION_ID || 'levanter_10da41c9b454e8453baf9992991d30b742').trim(),
   DATABASE:
     DATABASE_URL === databasePath
       ? new Sequelize({
